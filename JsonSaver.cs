15d6a2765896d5b15cad3e67f85babea5019b0a1
Mathe-man <mathieumousset6@gmail.com>
2024-11-14 14:07:50 +0300

    Initial commit

--- JsonSaver.cs
+++ JsonSaver.cs
@@ -1,8 +1,33 @@
-﻿using System;
+﻿using System.IO;
+using Newtonsoft.Json;
 
 namespace DataSaver
 {
-    public class JsonSaver
+    public static class JsonSaver
     {
+        public static void SaveToFile<T>(T content, string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))  //Only appen when the file is already existent
+                {
+                    // Serialize the content into Json format
+                    string jsonString = JsonConvert.SerializeObject(content, Formatting.Indented);
+                    // Write the json formated content in the specified file
+                    File.WriteAllText(filePath, jsonString);
+                }
+                else                        // if the file doesn't exist, it will be created and the function are recalled
+                {
+                    FileStream fs = File.Create(filePath);
+                    fs.Close();
+                    SaveToFile<T>(content, filePath);
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Directory.CreateDirectory(filePath.Remove(filePath.LastIndexOf("/")));
+                SaveToFile<T>(content, filePath);
+            }
+        }
     }
 }
